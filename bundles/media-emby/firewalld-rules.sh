f4e1242a14f8c6c9d73b5de987f391d8876e7839
julia <julia@volcanocoffee.monster>
2022-08-30 13:00:25 +0800

    Create firewalld-rules.sh

---
+++ bundles/media-emby/firewalld-rules.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# emby port(s)
+sudo firewall-cmd --permanent --zone=public --add-port=8096/tcp
+sudo firewall-cmd --permanent --zone=public --add-port=8920/tcp
+sudo firewall-cmd --permanent --zone=public --add-port=1900/udp
+sudo firewall-cmd --permanent --zone=public --add-port=7359/udp
+
+# jellyseerr port(s)
+sudo firewall-cmd --permanent --zone=public --add-port=8095/tcp
+
+# embystat port(s)
+sudo firewall-cmd --permanent --zone=public --add-port=6555/tcp
+
+# radarr port(s)
+sudo firewall-cmd --permanent --zone=public --add-port=8093/tcp
+
+# sonarr port(s)
+sudo firewall-cmd --permanent --zone=public --add-port=8092/tcp
+
+# firewalld reload
+sudo firewall-cmd --reload
