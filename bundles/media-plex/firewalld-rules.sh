36ca8f4e4386840d80c16512232657d590c2b5b0
julia <julia@volcanocoffee.monster>
2022-08-30 13:40:24 +0800

    Create firewalld-rules.sh

---
+++ bundles/media-plex/firewalld-rules.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# plex port(s)
+sudo firewall-cmd --permanent --zone=public --add-port=32400/tcp
+sudo firewall-cmd --permanent --zone=public --add-port=3005/tcp
+sudo firewall-cmd --permanent --zone=public --add-port=1900/udp
+sudo firewall-cmd --permanent --zone=public --add-port=5353/udp
+sudo firewall-cmd --permanent --zone=public --add-port=8324/tcp
+sudo firewall-cmd --permanent --zone=public --add-port=32410/udp
+sudo firewall-cmd --permanent --zone=public --add-port=32412/udp
+sudo firewall-cmd --permanent --zone=public --add-port=32413/udp
+sudo firewall-cmd --permanent --zone=public --add-port=32414/udp
+sudo firewall-cmd --permanent --zone=public --add-port=32469/udp
+
+# jellyseerr port(s)
+sudo firewall-cmd --permanent --zone=public --add-port=8095/tcp
+
+# embystat port(s)
+sudo firewall-cmd --permanent --zone=public --add-port=6555/tcp
+
+# radarr port(s)
+sudo firewall-cmd --permanent --zone=public --add-port=8093/tcp
+
+# sonarr port(s)
+sudo firewall-cmd --permanent --zone=public --add-port=8092/tcp
+
+# firewalld reload
+sudo firewall-cmd --reload
